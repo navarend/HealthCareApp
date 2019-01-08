2ed2d3578c01fb84ef77aa8e00dee00f0d2efe7e
Roberto <navarend@gmail.com>
2019-01-08 17:26:52 -0600

    add route and module customized in pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,37 +3,23 @@ import { NgModule } from '@angular/core';
 
 //routes
 import { APP_ROUTING } from './app.routes';
+//Modules
+import { PagesModule } from './pages/pages.module';
 
 import { AppComponent } from './app.component';
-
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { ChartsOneComponent } from './pages/charts-one/charts-one.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadCrumbsComponent } from './shared/bread-crumbs/bread-crumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    ChartsOneComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadCrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTING
+    APP_ROUTING,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
